b4fd936cda0118eb4322e474af8f5f21259453f9
neals-hub <j.eichler@neals.fr>
2023-10-26 17:02:40 +0200

    modif config github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://neals2023.github.io',
+  site: 'https://neals2023.github.io/neals2023',
+  base: '/neals2023',
   integrations: [tailwind()]
 });
